03a71f66e3bf2df101c4962b4a63e6b5db966389
geovaneedias <geovaneedias@gmail.com>
2025-06-29 18:17:05 -0300

    Update Control.java

--- Controller/Control.java
+++ Controller/Control.java
@@ -502,4 +502,15 @@ public class Control {
     public void setFacade(XadrezFacade facade) {
         this.facade = facade;
     }
+
+    public static void main(String[] args) {
+        try {
+            Control control = Control.getController();
+            XadrezFacade facade = new XadrezFacade(control);
+            facade.iniciarJogo();
+        } catch (Exception e) {
+            System.err.println("Erro ao iniciar o jogo: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
